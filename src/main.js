03d0d707baa76dad92a59cdb541b32272fef9d5b
Logivy <1327110430@qq.com>
2021-09-18 00:49:04 +0800

    解决300ms延迟问题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router';
 import VueLazyload from 'vue-lazyload'
 import store from './store';
+import toast from 'components/common/toast';
+
+var FastClick = require('fastclick');
+FastClick.attach(document.body);
 
 Vue.config.productionTip = false
 Vue.config.devtools = true;
@@ -12,6 +16,8 @@ Vue.use(VueLazyload, {
   preLoad: 1,
   loading: require('assets/img/common/placeholder.png')
 })
+
+Vue.use(toast)
 
 Vue.prototype.$bus = new Vue();
 
