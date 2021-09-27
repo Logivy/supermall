65c78bf2c659600d8f0ad2e0b03ed903bfbd2a28
Logivy <1327110430@qq.com>
2021-09-27 20:22:06 +0800

    [我的] 顶部组件

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="profile">
+    <nav-bar class="nav-bar">
+      <!-- v2.6.0 具名插槽写法 -->
+      <template v-slot:center>我的信息</template>
+      
+    </nav-bar>
   </div>
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar';
 export default {
   name: "Profile",
+  components:{
+    NavBar
+  },
+
 };
 </script>
 <style scoped>
+.nav-bar{
+  background-color: var(--color-tint);
+  color: #fff;
+  font-weight: 700;
+}
 </style>
