4f6beeffe5b0df6a0f9fa2e8e6f47d30834125c0
Logivy <1327110430@qq.com>
2021-09-29 14:24:08 +0800

    [个人信息] userinfo组件完成，使用矢量图标

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,24 @@
       <router-view />
     </keep-alive>
     <main-tab-bar></main-tab-bar>
+    <icon></icon>
+    <svg-icon></svg-icon>
   </div>
 </template>
 
 <script>
 import MainTabBar from "components/content/mainTabbar/MainTabBar";
 import NavBar from "components/common/navbar/NavBar";
+import Icon from "components/content/Icon/Icon.vue";
+import SvgIcon from "components/content/Icon/svg.vue";
 
 export default {
   name: "App",
   components: {
     MainTabBar,
     NavBar,
+    Icon,
+    SvgIcon,
   },
 };
 </script>
--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -3,16 +3,18 @@
     <nav-bar class="nav-bar">
       <!-- v2.6.0 具名插槽写法 -->
       <template v-slot:center>我的信息</template>
-      
     </nav-bar>
+    <user-info />
   </div>
 </template>
 <script>
 import NavBar from 'components/common/navbar/NavBar';
+import UserInfo from './childComps/UserInfo';
 export default {
   name: "Profile",
   components:{
-    NavBar
+    NavBar,
+    UserInfo
   },
 
 };
